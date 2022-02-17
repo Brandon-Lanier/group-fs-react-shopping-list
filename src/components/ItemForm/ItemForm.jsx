9af3565a3ff0fd750a9a976e78dea035051416f2
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-17 14:49:25 -0600

    Working on form

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -4,22 +4,26 @@ function ItemForm() {
     const [newName, setNewName] = useState('');
     const [newQuantity, setNewQuantity] = useState(0);  
     const [newUnit, setNewUnit] = useState(‘’);
+
+    const [newItem. setNewItem] = useState({name: '', quantity: 0, unit: ''})
     const [groceryList, setGroceryList] = useState([]);
 
-    const handleSubmit = () {
-        
+    const handleSubmit = (e) {
+        e.preventDefault();
+        addItem(new)
     }
 
 
     return (
         <div>
+            <form onSubmit={handleSubmnit}>
             <label htmlFor="name">Name</label>
-                <input value="name" placeholder="Name" />
+                <input value={newName} onChange={(e) => setNewItem({e.target.value)} />
             <label htmlFor="quantity">Quantity</label>
                 <input value="quantity" placeholder="Quantity"/>
             <label htmlFor="unit">Unit</label>
-            <button onClick={handleSubmit}>Save</button>
-                
+            <input type="submit" value="Submit"/>
+            </form>
 
 
         </div>
